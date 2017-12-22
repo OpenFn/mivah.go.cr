8cc11779d7b2e7ded372e2c46d92b596a868d4dd
Santiago Gutierrez <santiago@openfn.org>
2017-12-22 16:56:21 -0300

    Update post-to-asp-system.js

--- post-to-asp-system.js
+++ post-to-asp-system.js
@@ -1,7 +1,8 @@
 post("/asp.net/webhooks", {
      body: {
-       "some": "data",
-       "some_more": state.data.participant_age
+       "some_data": "data",
+       "some_other_data": state.data.participant_age,
+       "some_more_data": dataValue("group1/name")
      },
      headers: {"content-type": "json"}
 });
